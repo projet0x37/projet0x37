44e1d92818ddb2d4dcbeb4ee17a5879d4ca87425
Projet0x37 <projet0x37@gmail.com>
2016-03-30 18:27:36 +0200

    corrige variable taudelay dans noteoff

--- MIDI/midi.c
+++ MIDI/midi.c
@@ -93,7 +93,7 @@ FILE * miditrackdata( Tnote tab , int tailletab , char * trackdata ){
 			tau = tab[i+1].temps-tab[i].temps
 			Si = split(Li,tau);
 			for( p=Li ; p->suiv ; p=p->suiv );
-			taudelay = tab[i+1].temps - p->duree ; // a faire avant le noteoff ET le split
+			taudelay = tau - p->duree ; // a faire avant le noteoff ET le split
 			noteoff( Li , Si , miditrackdata ); // l'ajustement temporel des listes s'effectue dans note off et delay
 			delay( Si , taudelay , miditrackdata) ;
 			freelist(Li); // c'était une copie d'une liste de l'étape ti , libère la mémoire
