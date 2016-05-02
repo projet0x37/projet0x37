b8754bf4dd2d6fdcbcf1c4a8a1f67ade6581492c
NOEPG <paul.gauthier.funkys@gmail.com>
2016-05-02 17:42:43 +0200

    Ajout de la fonction F0fromL

--- traitement/fonctions.c
+++ traitement/fonctions.c
@@ -75,8 +75,9 @@ double * Z_calc(double * Y, double * N, int taille){
 void fosup(frame Zsmoothed, frame Z, int taille ){
 	int i;
 	for(i=0;i<taille;i++){
-		Z[i]=Z[i]-Zsmoothed[i] ; }
+		Z[i]=Z[i]-Zsmoothed[i] ;
 	}
+}
 
 int processing_init(frame X, frame Lmax,frame Npow, int taille, double threshold,int k0,int k1){
 	double Lmax1=0;	
@@ -86,13 +87,15 @@ int processing_init(frame X, frame Lmax,frame Npow, int taille, double threshold
 	double vo=0;
 	for(i=0;i<taille;i++){
 		Lmax1+=Lmax[i] ;
+	}
 	for(i=k0;i<k1;i++){
 		x+=X[i] ;
 		N+=Npow[i];
+	}
 	vo=4*ln(Lmax1)+ln(x)-ln(N) ;
 	if (vo>threshold) return 1;
 	if (vo<threshold) return 0;
-	
+	}
 
 int iteration_checking(frame X, frame Lmaxi,frame Npow, int taille,double treshold, int k0, int k1){
 	double Lmax1=0;	
@@ -102,10 +105,30 @@ int iteration_checking(frame X, frame Lmaxi,frame Npow, int taille,double tresho
 	double vo=0;
 	for(i=0;i<taille;i++){
 		Lmax1+=Lmaxi[i] ;
+	}
 	for(i=k0;i<k1;i++){
 		x+=X[i] ;
 		N+=Npow[i];
+	}
 	vo=1.8*ln(Lmax1)+ln(x)-ln(N) ;
 	if (vo>threshold) return 1;
 	if (vo<threshold) return 0;
 	
+}
+
+int F0fromL(double* L, int taille){			//Testée et approuvée
+	int i;
+	double M=-1;
+	int fM;
+	if(!L){
+		puts("Erreur : L est vide");
+		return(0);
+	}
+	for(i=0;i<taille;i++){
+		if(L[i]>M){
+			M=L[i];
+			fM = i+1;
+		}
+	}
+	return(fM);
+}
