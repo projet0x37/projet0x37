982c5c3d832046d50d2109d5f0aad6a46e89683d
Gabriel <g.destouet@hotmail.fr>
2016-05-05 21:00:03 +0200

    modifie le main , encore à l'état de brouillon, les tests ont été effectués avec la libraire fftw, et permettent d'évaluer les différences avec les transformées de Fourier sous scilab

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "traitement/fonctions.h"
 #include "midi/midi.h"
 #include "textexport/textexport.h"
-
+#include <fftw3.h>
 
 int main(int argc, char** args){
 	double * datain;
@@ -13,23 +13,54 @@ int main(int argc, char** args){
 	int  size;
 	double duration;
 	double samplerate;
-	int minframelength = 4096 ;
+	int frames = 8192 ;
 	double minframeduration;
 	int sizeTmax;
 	double timeresolutionsms;
-
+	fftw_complex * out;
+	fftw_plan p;
+	double * DSP;
+	int i;
+	double * in;
+	int sizeout;
+	int position;
+	double t0;
+	double tf;
 
 	datain=mainaudio("input.wav",&size,&samplerate);
 
+	in = fftw_malloc( sizeof(double)*frames);
+	sizeout = frames/2 + 1;
+	out = fftw_malloc(sizeof(fftw_complex)*sizeout);
+	p = fftw_plan_dft_r2c_1d( frames , in , out , FFTW_ESTIMATE );
+
+	DSP=calloc(sizeout,sizeof(double));
+
 	duration = (double)size/samplerate;
-	minframeduration = (double)4096/samplerate;
-	sizeTmax=2*floor(size/4096) + 2; // on prend une taille suffisamment large
+	minframeduration = (double)frames/samplerate;
+	sizeTmax = 2*floor(size/4096) + 2; // on prend une taille suffisamment large
 	
 	printf("Le fichier audio dure %lf s , il comporte %d éléments, la fréquence d'échantillonage est de %lf, en conséquence le tableau Tnote à %d éléments\n",duration,size,samplerate,sizeTmax);
 	
-	textexport("datainoutput",datain,size,size);
+	printf("Entrer le position du début de la porte ( sachant qu'on se décale d'une demie porte )\n");
+	scanf("%d",&position);
 
-	
+	for(i=0 ; i < frames ; i++){
+		if (i + position*frames/2 < size)in[i] = datain[ i + position*frames/2 ];
+		else in[i] = 0;
+	}
+	t0 = position*frames/(2*samplerate);
+	tf = t0 + frames/samplerate;
+	printf("T0 %lf , Tf %lf \n",t0,tf);
+	fftw_execute(p);
+
+	for(i=0;i<sizeout;i++) DSP[i] = ( out[i][0]*out[i][0] + out[i][1]*out[i][1] );
+
+	textexport("datainoutput",in,frames,frames);
+	textexport("DSPout",DSP,sizeout,sizeout);
+
+	fftw_free(out);
+	fftw_destroy_plan(p);
 
 	/*
 	T=calloc( sizeTmax , sizeof(*T) );
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC=gcc
 CFLAGS= -c -g -Wall
-LFLAGS = -L audio/. audio/libsndfile.a -lm
+LFLAGS = -L audio/. audio/libsndfile.a -lm -lfftw3
 
 OBJECTS = audio/audioprocess.o main.o midi/listemidi.o midi/midi.o traitement/fonctions.o textexport/textexport.o
 
