344758e0d777628698818fb0307962d8a315aa30
Gabriel <g.destouet@hotmail.fr>
2016-05-10 16:12:22 +0200

    met a jour textexport.h

--- textexport/textexport.h
+++ textexport/textexport.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
 
 void textexport(char * textname,double * data,int taille,int taille_utilisateur);
+void textexport_Mat(char * textname , double ** data , int colonne, int ligne);
 double* tabexportfromtext(char * textname,int nb);
